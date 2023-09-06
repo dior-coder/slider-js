9a086b36407cfcb5cc90d4b9c56705e6d1d1342c
Dior Ulmasbaev <diyor.nabijonov.2001@gmail.com>
2023-09-07 01:04:57 +0600

    slider js

---
+++ script/slider.js
@@ -0,0 +1,6 @@
+$("#slider").slick({
+    infinity: true,
+    mobileFirst: false,
+    prevArrow: "<div class='slick-own-btn own-btn-prev'>&larr;</div>",
+    nextArrow: "<div class='slick-own-btn own-btn-next'>&rarr;</div>",
+  });
